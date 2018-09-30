92d411f6a2aa9712bbe17a8d273dd714b7b27522
Александр Полторацкий <pag.2010@ya.ru>
2018-09-30 21:27:28 +0300

    Добавил метаданные в строковом формате
    List хранит полное название архивируемых файлов

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 var Path string
 var Output string
 var Mode string
+var List []string
 
 //var FileNames string
 
@@ -42,6 +44,11 @@ func main() {
 			log.Fatal(err)
 		}
 		log.Println("Zipped File: " + output)
+		p := Path + "\\"
+		for _, file := range List {
+			file = strings.Replace(file, p, "", -1)
+			log.Printf(file)
+		}
 	default:
 		log.Fatal("Unknown code for mode")
 	}
@@ -56,19 +63,19 @@ func ZipFiles(path string, zipWriter *zip.Writer, dirName string) error {
 	for _, file := range files {
 		log.Printf(file.Name(), file.IsDir())
 		if file.IsDir() {
-			_, err := zipWriter.Create(dirName + file.Name() + "/")
+			_, err := zipWriter.Create(dirName + file.Name() + "\\")
 			if err != nil {
 				return err
 			}
-			ZipFiles(path+"/"+file.Name(), zipWriter, dirName+file.Name()+"/")
+			ZipFiles(path+"\\"+file.Name(), zipWriter, dirName+file.Name()+"\\")
 		} else {
-
+			List = append(List, path+"\\"+file.Name())
 			writer, err := zipWriter.Create(dirName + file.Name())
 			if err != nil {
 				return err
 			}
 
-			data, err := ioutil.ReadFile(path + "/" + file.Name())
+			data, err := ioutil.ReadFile(path + "\\" + file.Name())
 			if err != nil {
 				return err
 			}
